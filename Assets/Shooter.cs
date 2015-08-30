702e21061741eb4a9f7ae06b639d9ce2584b806e
Eric Smith <paytonrules@gmail.com>
2015-08-30 13:20:26 -0500

    Movement, and you can shoot and get a sphere to show up

---
+++ Assets/Shooter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		_camera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			var point = new Vector3(_camera.pixelWidth / 2, 
+			                        _camera.pixelHeight / 2,
+			                        0);
+			var ray = _camera.ScreenPointToRay(point);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				StartCoroutine(SphereIndicator(hit.point));
+			}
+		}
+	}
+
+	private IEnumerator SphereIndicator(Vector3 pos) {
+		var sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		sphere.transform.position = pos;
+
+		yield return new WaitForSeconds (1);
+
+		Destroy (sphere);
+	}
+}
