b15ef9ac4dc9e7834b6c7441e108d5158abe5abe
Eric Smith <paytonrules@gmail.com>
2015-08-30 19:11:37 -0500

    Add to the HUD

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -7,6 +7,8 @@ public class Shooter : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		_camera = GetComponent<Camera> ();
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
@@ -21,6 +23,13 @@ public class Shooter : MonoBehaviour {
 				StartCoroutine(SphereIndicator(hit.point));
 			}
 		}
+	}
+
+	void OnGUI() {
+		int size = 12;
+		float posX = _camera.pixelWidth / 2 - size / 4;
+		float posY = _camera.pixelHeight / 2 - size / 2;
+		GUI.Label (new Rect (posX, posY, size, size), "*");
 	}
 
 	private IEnumerator SphereIndicator(Vector3 pos) {
